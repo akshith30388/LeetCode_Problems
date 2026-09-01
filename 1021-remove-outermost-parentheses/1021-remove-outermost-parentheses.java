29086a9ce3120963a85a6de02bfa4f47d48ac4da
Akshith <2400030388@kluniversity.in>
2026-09-01 22:44:49 +0530

    Time: 2 ms (99.74%), Space: 43.8 MB (26.94%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count=0;
+        StringBuilder str=new StringBuilder();
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                if(count>0){
+                str.append(ch);
+                }
+                count++;
+            }else{
+                count--;
+                if(count>0){
+                    str.append(ch);
+                }
+            }
+        }
+        return str.toString();
+    }
+}
+
+
+
